b0c7e8c73ae411772186141b08c5c1fb038bfa42
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-08-31 14:45:22 +0200

    Generateur de "continue" a la fin de chaque partie

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello Wo...Hangman !")
+print("Bienvenue dans le pendu !")
+continuer = True
+
+while continuer == True:
+    nouv_partie = input("Veux-tu reprendre une nouvelle partie ? (réponds \"oui\" si tu veux continuer, n'importe quoi d'autre pour arrêter :\n> ")
+    
+    nouv = nouv_partie.lower()
+    if nouv != "oui":
+        continuer = False
+        print("Très bien, merci d'avoir joué !")
+    else:
+        print("Super, on s'en refait une !")
+
