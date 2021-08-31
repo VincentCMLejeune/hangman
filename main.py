c1e31ac470208d1bad089f756d4cd92265453d15
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-08-31 14:53:10 +0200

    Entrée du mot à deviner

--- main.py
+++ main.py
@@ -2,12 +2,36 @@ print("Bienvenue dans le pendu !")
 continuer = True
 
 while continuer == True:
-    nouv_partie = input("Veux-tu reprendre une nouvelle partie ? (réponds \"oui\" si tu veux continuer, n'importe quoi d'autre pour arrêter :\n> ")
-    
-    nouv = nouv_partie.lower()
-    if nouv != "oui":
-        continuer = False
-        print("Très bien, merci d'avoir joué !")
-    else:
-        print("Super, on s'en refait une !")
 
+# Ajout du mot à deviner et dissimulation de la console
+    mot_a_lowercaser = input("Joueur 1, inscris le mot à deviner :\n> ")
+    mot = mot_a_lowercaser.lower()
+    print(mot)
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("Suis-je bien caché ?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Nouvelle partie ?
+    # nouv_partie = input("Veux-tu reprendre une nouvelle partie ? (réponds \"oui\" si tu veux continuer, n'importe quoi d'autre pour arrêter :\n> ")
+    # nouv = nouv_partie.lower()
+    # if nouv != "oui":
+    #     continuer = False
+    #     print("Très bien, merci d'avoir joué !")
+    # else:
+    #     print("Super, on s'en refait une !")
